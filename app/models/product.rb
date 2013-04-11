40098b9b60a822170aad693819d29900ec4b6119
Jeffrey Kao <jeffthestampede@gmail.com>
2013-04-10 20:02:42 -0700

    first commit

---
+++ app/models/product.rb
@@ -0,0 +1,5 @@
+class Product < ActiveRecord::Base
+  attr_accessible :name, :price, :reviews
+
+  has_many :reviews
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsStore::Application.config.secret_token = '6818769622da938f9c3c500e7386cb18e595938d8ea55919c4e3ebd743a2bb68249875a300d544460f843b3385f64591fb7f0ccc1707a7e44159f16c31963315'
