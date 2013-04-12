e7f72772b045b52426e5b2869da410eeeefaceb4
Jeffrey Kao <jeffthestampede@gmail.com>
2013-04-11 20:43:55 -0700

    add cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  attr_accessible :name, :price, :reviews
+  attr_accessible :name, :price, :reviews, :imageurl, :incart
   
   validates_uniqueness_of :name
   #validates_inclusion_of :price
